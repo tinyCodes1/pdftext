ad42bce3ad2420b8f9c6efd8999e30eee4e5f821
Tiny Apps <tinyapps1@gmail.com>
2024-10-12 10:34:39 +0530

    resolved error

--- pdftxt.ts
+++ pdftxt.ts
@@ -7,7 +7,7 @@
 import {pdfText} from "./pdftext.ts";
 import { SEPARATOR } from "./src/sep.ts";
 
-const version = "1.1.9"
+const version = "1.2.0"
 const showHelp=()=> {
   const parts = Deno.mainModule.split(`/`);
   const scriptName = parts[parts.length -1];
